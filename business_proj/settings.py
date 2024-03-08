08bdc6d1536649120a3db4775f7844ebab715d66
mikegyamfi <codeage20@gmail.com>
2024-03-08 14:59:49 +0000

    secrets

--- business_proj/settings.py
+++ business_proj/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     "plankton-app-s46li.ondigitalocean.app",
     'lobster-app-jqrdy.ondigitalocean.app',
-    'merchant.cloudhubgh.com'
+    'merchant.cloudhubgh.com',
     'localhost',
     '127.0.0.1'
 ]
